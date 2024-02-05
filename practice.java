3d7d1dc0a4c92b5ac448b4bbe8c447338b60425c
Eldrie Yancy C. Icoy <158005676+EldrieYancyIcoy@users.noreply.github.com>
2024-02-05 10:46:13 +0800

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+     public static void main(String[] args){
+       Scanner scanner= new Scanner (System.in);
+       int number;
+       //int sum;
+       //sum= a+b;
+       //System.out.println("Sum ="+sum);
+
+       system.out.println(x:"Enter your values");
+       number = input.nextInt();
+       if(number>)
+
+    }
+}
